965b4550fbd9dc408a5ad194d6ac39bf59a17415
Lucas 'Lucaspec72' Pecquenard <48445530+Lucaspec72@users.noreply.github.com>
2024-05-07 11:38:58 +0200

    update

---
+++ Ksp-EnergyDrainer-EngineModule/Ksp-EnergyDrainer-EngineModule/ModuleEnergyDrain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Ksp_EnergyDrainer_EngineModule
+{
+    public class ModuleEnergyDrain : PartModule
+    {
+        [KSPField(isPersistant = true)]
+        public ModuleEngines EngineModule;
+
+        //Guiname and GuiActive are tests to see if they show up.
+        [KSPField(guiName = "Target Ressource", guiActive = true, isPersistant = false)]
+        public string ressourceDrained;
+        [KSPField(guiName = "Base Consumption", guiActive = true, isPersistant = false)]
+        public float consumption;
+        [KSPField(guiName = "Current Throttle", guiActive = true, isPersistant = false)]
+        public float currentThrottle;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+            {
+                if (state != StartState.Editor && state != StartState.None)
+                {
+                    this.enabled = true;
+                    this.part.force_activate();
+                    EngineModule = this.part.Modules.OfType<ModuleEngines>().Single();
+                }
+                else
+                {
+                    this.enabled = false;
+                }
+            }
+        }
+
+
+
+        public override void OnFixedUpdate()
+        {
+            currentThrottle = EngineModule.currentThrottle;
+        }
+
+    }
+}
