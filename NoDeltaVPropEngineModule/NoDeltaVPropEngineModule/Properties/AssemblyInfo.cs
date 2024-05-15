fbf33e9cd414df456e797e888db8d9c1c787883d
Lucas 'Lucaspec72' Pecquenard <48445530+Lucaspec72@users.noreply.github.com>
2024-05-15 15:38:10 +0200

    change project name
    finish changing all references to Ksp-EnergyDrainer-EngineModule to NoDeltaVPropEngineModule

--- NoDeltaVPropEngineModule/NoDeltaVPropEngineModule/Properties/AssemblyInfo.cs
+++ NoDeltaVPropEngineModule/NoDeltaVPropEngineModule/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Ksp-EnergyDrainer-EngineModule")]
+[assembly: AssemblyTitle("NoDeltaVPropEngineModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ksp-EnergyDrainer-EngineModule")]
+[assembly: AssemblyProduct("NoDeltaVPropEngineModule")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
